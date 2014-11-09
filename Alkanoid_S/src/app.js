a210bbb9afb718e6842af76c7186ea0a01e076a7
parksooyoung <yanarister@naver.com>
2014-11-09 15:22:25 +0900

    수

--- Alkanoid_S/src/app.js
+++ Alkanoid_S/src/app.js
@@ -9,28 +9,38 @@ var HelloWorldLayer = cc.Layer.extend({
 		this.ball = new cc.Sprite("res/ball2.png");
 		this.ball.scale = 0.005;
 		this.ball.x = this.width/2;
-		this.ball.y = this.height/2;
+		this.ball.y = 70;
 		this.addChild(this.ball);
 		var initialDirection = 4.5;
 		this.ball.speedX = 6*Math.cos(initialDirection);
 		this.ball.speedY = 6*Math.sin(initialDirection);
 		this.ball.angleSpeed = 0;
 		this.box = new cc.Sprite("res/box.png");
-		this.box.scaleX = 0.15;
+		this.box.scaleX = 0.1;
 		this.box.scaleY = 0.04;
 		this.box.x = this.width/2;
 		this.box.y = 50;
 		this.addChild(this.box);
 		this.box_g = new cc.Sprite("res/box_g.png");
-		this.box_g.scaleX = 0.04;
-		this.box_g.scaleY = 0.3;
+		this.box_g.scaleX = 0.08;
+		this.box_g.scaleY = 0.08;
 		this.box_g.x = this.width/2;
 		this.box_g.y = 300;
 		this.addChild(this.box_g);
+		
+		var string = "Life : " + life.toString();
+		var label = cc.LabelTTF("", "Arial", 30);
+		label.setString(string);
+		label.x = 700;
+		label.y = 400;
+		this.addChild(label);
+		
+	
 		var eventListener = cc.EventListener.create({
 			event:cc.EventListener.TOUCH_ONE_BY_ONE,
 			swallowTouches:true,
 			onTouchBegan:function(){
+				
 				return true;
 			},
 			onTouchMoved:function(touch, event){
@@ -39,9 +49,16 @@ var HelloWorldLayer = cc.Layer.extend({
 			}
 		});
 		cc.eventManager.addListener(eventListener, this);
+		//this.setKeyboardEnabled(true);
+
 		this.update = function(){
 			
-			//만들어진 이미지를 뿌리는걸로. Life : 3 대충 이런 이미지를 점수의 경우는... 음; 
+			var string = "Life : " + life.toString();
+			label.setString(string);
+			
+			/*if(life == 0){
+				onKeyDown();
+			}*/
 			
 			//바 
 			var boxTop = this.box.y+this.box.getBoundingBox().height/2;
@@ -97,7 +114,8 @@ var HelloWorldLayer = cc.Layer.extend({
 			}
 			//초록박스 아랫쪽 튕김 
 			if(ballTop<box_g_Bottom && ballTop + this.ball.speedY > box_g_Bottom){//초록 박스의 아랫부분 충돌처리 
-				var xOnBox = this.ball.x+(box_g_Bottom+this.ball.getBoundingBox().height/2-this.ball.y)*this.ball.speedX/(this.ball.speedY);
+				cc.log("bottom");
+				var xOnBox = this.ball.x+(box_g_Bottom-this.ball.getBoundingBox().height/2-this.ball.y)*this.ball.speedX/(this.ball.speedY);
 				var boxLeft = this.box_g.x-this.box_g.getBoundingBox().width/2;
 				var boxRight = this.box_g.x+this.box_g.getBoundingBox().width/2;
 				if(xOnBox>boxLeft&&xOnBox<boxRight){
@@ -110,16 +128,14 @@ var HelloWorldLayer = cc.Layer.extend({
 						this.ball.angleSpeed = -1.2;
 					}
 					this.ball.y = 2*(box_g_Bottom - this.ball.getBoundingBox().height/2)-this.ball.y;
-					var alpha = Math.atan2(this.ball.speedY, this.ball.speedX);
-					alpha += 0.01*(this.box_g.x-xOnBox);
-					this.ball.speedX = 6*Math.cos(alpha);
-					this.ball.speedY = 6*Math.sin(alpha);
+
 				}
+				
 			}
 			//초록박스 왼쪽 튕김 
-			if(ballRight > box_g_Left && ballRight + this.ball.speedX < box_g_Right){
-				cc.log("Left");
-				var xOnBox = this.ball.y + (box_g_Left + this.ball.getBoundingBox().width/2 - this.ball.x)*this.ball.speedX/(this.ball.speedY);
+			if(ballRight < box_g_Left && ballRight + this.ball.speedX > box_g_Left){
+				//cc.log("Left");
+				var xOnBox = this.ball.y + (box_g_Left - this.ball.getBoundingBox().width/2 - this.ball.x)*this.ball.speedY/(this.ball.speedX);
 				var box_Top = this.box_g.y + this.box_g.getBoundingBox().height/2;
 				var box_Bottom = this.box_g.y - this.box_g.getBoundingBox().height/2;
 				if(xOnBox < box_Top && xOnBox > box_Bottom){
@@ -138,9 +154,9 @@ var HelloWorldLayer = cc.Layer.extend({
 				}
 			}
 			//초록박스 오른쪽 튕김 
-			if(ballLeft < box_g_Right && ballLeft + this.ball.speedX > box_g_Left){
-				cc.log("Right");
-				var xOnBox = this.ball.y + (box_g_Right + this.ball.getBoundingBox().width/2 - this.ball.x)*this.ball.speedX/(this.ball.speedY);
+			if(ballLeft > box_g_Right && ballLeft + this.ball.speedX < box_g_Right){
+				//cc.log("Right");
+				var xOnBox = this.ball.y + (box_g_Right + this.ball.getBoundingBox().width/2 - this.ball.x)*this.ball.speedY/(this.ball.speedX);
 				var box_Top = this.box_g.y + this.box_g.getBoundingBox().height/2;
 				var box_Bottom = this.box_g.y - this.box_g.getBoundingBox().height/2;
 				if(xOnBox < box_Top && xOnBox > box_Bottom){
@@ -192,6 +208,8 @@ var HelloWorldLayer = cc.Layer.extend({
 				this.ball.y = 2*(this.ball.getBoundingBox().height/2)-this.ball.y;*/
 					this.ball.x = this.width/2;
 					this.ball.y = this.height/2;
+					if(life>0)
+						life--;
 				}
 				if(this.ball.y>this.height-this.ball.getBoundingBox().height/2){
 					this.ball.speedY *= -1;
@@ -206,7 +224,21 @@ var HelloWorldLayer = cc.Layer.extend({
 			
 		this.scheduleUpdate();
 		return true;
-	}
+	}/*,
+	onKeyDown:function(e){
+		switch (e) {
+		case cc.Key.enter:{
+			var initialDirection = 4.5;
+			this.ball.speedX = 6*Math.cos(initialDirection);
+			this.ball.speedY = 6*Math.sin(initialDirection);
+			this.ball.angleSpeed = 0;
+			this.ball.x = this.width/2;
+			this.ball.y = this.height/2;
+			life = 3;
+			break;
+			}
+		}
+	}*/
 });
 var HelloWorldScene = cc.Scene.extend({
 	onEnter:function () {
